cb1f0add4aeb6bfada539b49e0b95d31eedab222
sans.code <sanjari.code@gmail.com>
2023-04-27 21:16:03 +0700

    First Commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <div id="content">
+      <iframe src="https://mez.ink/sanjari.code" width="100%" height="100%" frameborder="0"></iframe>
+    </div>
+  </q-page>
+</template>
+
+<style>
+body,
+html {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  overflow: hidden;
+}
+
+#content {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  top: 0px;
+}
+</style>
